8ec82b00521156c54f4008c1401dbb59ef37432e
AshokaTatangS <nullfier14@gmail.com>
2024-11-28 00:57:42 +0700

    Pesan commit Anda

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: React.FC = () => {
           <h1 className="text-2xl font-bold text-white">PortalPosyandu</h1>
         </div>
 
-        {/* Navigation Links, border hanya pada bagian bawah */}
+        {/* Navigation Links */}
         <ul className="space-y-1 mt-4 border-r border-grey-300 h-full">
           <li>
             <Link
@@ -25,7 +25,56 @@ const Navbar: React.FC = () => {
                 "/"
               )}`}
             >
-              Kalender
+              Beranda
+            </Link>
+          </li>
+          {/* Catatan Kesehatan (non-clickable) */}
+          <li>
+            <div className="block py-3 px-4 text-gray-700 bg-white cursor-default">
+              Catatan Kesehatan
+            </div>
+          </li>
+          {/* Submenu for Catatan Kesehatan */}
+          <ul className="ml-6 space-y-1">
+            <li>
+              <Link
+                to="/bayi"
+                className={`block py-2 px-4 text-gray-600 hover:bg-pink-50 ${isActive(
+                  "/bayi"
+                )}`}
+              >
+                Bayi dan Balita
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/catatan-kesehatan/ibu-hamil"
+                className={`block py-2 px-4 text-gray-600 hover:bg-pink-50 ${isActive(
+                  "/catatan-kesehatan/ibu-hamil"
+                )}`}
+              >
+                Ibu Hamil
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/catatan-kesehatan/lansia"
+                className={`block py-2 px-4 text-gray-600 hover:bg-pink-50 ${isActive(
+                  "/catatan-kesehatan/lansia"
+                )}`}
+              >
+                Lansia
+              </Link>
+            </li>
+          </ul>
+          <li>
+            <Link
+              to="/laporan-posyandu"
+              className={`block py-3 px-4 text-gray-700 hover:bg-pink-100 ${isActive(
+                "/laporan-posyandu"
+              )}`}
+            >
+              Laporan Posyandu
             </Link>
           </li>
           <li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,29 @@ import Navbar from "./components/Navbar";
 import Kalender from "./components/Kalender";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Bayi from "./components/Bayi";
+import TambahBayi from "./components/TambahBayi";
+import { DataProvider } from "./components/DataContext"; // Import DataProvider
 
 function App() {
   return (
-    <Router>
-      <div className="flex mt-10">
-        <Navbar />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<Kalender />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+    // Bungkus seluruh aplikasi dengan DataProvider
+    <DataProvider>
+      <Router>
+        <div className="flex mt-10">
+          <Navbar />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<Kalender />} />
+              <Route path="/bayi" element={<Bayi />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/TambahBayi" element={<TambahBayi />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </DataProvider>
   );
 }
 
